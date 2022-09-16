14c6b530e2d9a51ae026a5c9e61c07fc14a2947b
prarthit <prarthitmehra@gmail.com>
2022-09-16 13:34:42 -0700

    Add getPositions method

--- src/main/java/cecs429/indexing/Posting.java
+++ src/main/java/cecs429/indexing/Posting.java
@@ -21,6 +21,10 @@ public class Posting {
 		mPositions.add(position);
 	}
 
+	public List<Integer> getPositions() {
+		return mPositions;
+	}
+
 	public int getDocumentId() {
 		return mDocumentId;
 	}
